9f2a0c48a02da480d206304cab9e5d1a53c77603
Artem Rybin <arybin93@gmail.com>
2018-03-27 15:14:36 +0300

    Feature - igw coeffs

--- tasks/igwcoeffs.py
+++ tasks/igwcoeffs.py
@@ -4,11 +4,12 @@ from scipy import integrate
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 from constance import PI
+from scipy.integrate import odeint
 
 num_mode = 1
 max_mode = 1
 
-epsilon_f = 1e-7
+epsilon_f = 1e-3
 epsilon_t = 1e-4
 
 epsilon_t_rough = 1e-2
@@ -22,10 +23,9 @@ def read_file(fname, path=r"C:\Users\Rybin\PycharmProjects\sandbox\resources\dat
     return data
 
 
-def sys_phi(zz, y, c, N):
-    dy = np.zeros(2)
-    dy[0] = y[1]
-    dy[1] = -((N(zz)/c)**2)*y[0]
+def sys_phi(y, z, N, c):
+    phi, dphi = y
+    dy = [dphi, -((N(z) / c) ** 2) * phi]
     return dy
 
 
@@ -53,7 +53,7 @@ def calc_coeffs_point():
         rev_z[len_data-1-i] = max_depth - z_down[i]
         rev_bvf[len_data-1-i] = n_freq[i]
 
-    N = interp1d(rev_z, rev_bvf, kind='cubic')
+    N = interp1d(rev_z, rev_bvf, kind='cubic', fill_value="extrapolate")
 
     c = np.zeros(2)
     for i in range(0, max_mode):
@@ -64,51 +64,27 @@ def calc_coeffs_point():
 
         z_zer = np.zeros(max_mode + 1)
         iter = 0
-        z = np.linspace(rev_z[0], rev_z[-1], 1000)  # the points of evaluation of solution
+        z = np.linspace(rev_z[0], rev_z[-1], 1500)  # the points of evaluation of solution
         init_cond = [0, 0.01]  # initial value
+
         while True:
-            solver = integrate.ode(sys_phi).set_integrator('dopri5', rtol=1e-4, atol=[1e-6])
-            solver.set_f_params(c[i], N)
-            solver.set_initial_value(init_cond, rev_z[0])
-
-            # Additional Python step: create vectors to store trajectories
-            z_grid = np.zeros((len(z), 1))
-            phi = np.zeros((len(z), 1))
-            dPhi = np.zeros((len(z), 1))
-            phi[0] = 0
-            dPhi[0] = 0
-
-            # Integrate the ODE(s) across each delta_z
             zero_counter = 0
-            for k in range(1, z.size):
-                res = solver.integrate(z[k])
+            sol = odeint(sys_phi, init_cond, z, args=(N, c[i]), rtol=[1e-4, 1e-4], atol=[1e-6, 1e-6])
+            phi = sol[:, 0] * 10
+            dphi = sol[:, 1]
 
-                if not solver.successful():
-                    raise RuntimeError("Could not integrate")
-
-                # Store the results
-                z_grid[k] = z[k]
-                phi[k] = res[0]
-                dPhi[k] = res[1]
-
-            #plt.plot(phi, z_grid)
-            #plt.gca().invert_yaxis()
-            #plt.show()
-
-            n_z_grid = len(z_grid) - 1
+            n_z_grid = len(z) - 1
             for j in range(1, n_z_grid):
-                if phi[j-1] * phi[j] < 0:
+                if phi[j - 1] * phi[j] < 0:
                     zero_counter += 1
-                    z_zer[zero_counter] = z_grid[j - 1]
+                    z_zer[zero_counter] = z[j - 1]
             z_zer_Phi = z_zer[i]
 
             print(iter)
             print('c = {}'.format(c[i]))
-            print(np.abs(phi[n_z_grid]/max(phi)))
-            print(epsilon_f)
             iter += 1
 
-            if np.abs(phi[n_z_grid]/max(phi)) <= epsilon_f:
+            if np.abs(phi[n_z_grid] / max(phi)) <= epsilon_f:
                 break
             elif dc < 1e-10:
                 raise RuntimeError("Could not integrate")
@@ -118,3 +94,7 @@ def calc_coeffs_point():
             elif zero_counter > i:
                 dc /= 2
                 c[i] += dc
+
+    plt.plot(phi, z)
+    plt.gca().invert_yaxis()
+    plt.show()
